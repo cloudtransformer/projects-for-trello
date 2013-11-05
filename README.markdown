6b93207c4cd20eb8cb8ab41d1db82d30f259aeaa
Dan Lister <d.lister@agebase.co.uk>
2013-11-05 16:55:47 +0000

    Cleaned source and added readme content.

---
+++ README.markdown
@@ -0,0 +1,25 @@
+Projects for Trello
+===========
+Projects for Trello adds functionality to the awesome trello.com for use in multiple project boards.
+
+Setup
+-----
+Projects for Trello is a Chrome extension and you can install it via the Chrome Webstore or clone this 
+repository and load the Projects for Trello folder as an unpacked extension.
+
+How does it work?
+-----------------
+Trello Projects is a Chrome Extension that adds a little bit of functionality
+to Trello.com to make it perfect to use with multiple projects. Trello Projects
+allows you to add project labels to cards.
+
+Project names are written in between parentheses in the title of a card.
+For example: {Project} Upon an error I want to see an awesome 404 page.
+Upon saving the card, Trello Projects picks up the assigned project name and shows it
+as a badge in the lower left corner of the card.
+
+Credits
+-------
+Most, if not all credit goes to [TrelloScrum](https://github.com/Q42/TrelloScrum) which was 
+was developed by [Marcel Duin](http://webglmarcel.q42.net/) and [Jasper Kaizer](https://twitter.com/jkaizer). 
+This project is a re-work of their original extension to handle project labels.
--- trelloprojects.js
+++ trelloprojects.js
@@ -1,158 +1,114 @@
-/*
-** Scrum for Trello- https://github.com/Q42/TrelloScrum
-** Adds Scrum to your Trello
-**
-** Original:
-** Jasper Kaizer <https://github.com/jkaizer>
-** Marcel Duin <https://github.com/marcelduin>
-**
-** Contribs:
-** Paul Lofte <https://github.com/paullofte>
-** Nic Pottier <https://github.com/nicpottier>
-** Bastiaan Terhorst <https://github.com/bastiaanterhorst>
-** Morgan Craft <https://github.com/mgan59>
-** Frank Geerlings <https://github.com/frankgeerlings>
-** Cedric Gatay <https://github.com/CedricGatay>
-** Kit Glennon <https://github.com/kitglen>
-** Samuel Gaus <https://github.com/gausie>
-**
-*/
-
-//default story point picker sequence
-
-
-//internals
-var regp = /((?:^|\s))\{([a-zA-Z0-9\s]+)(\})\s?/m; //parse regexp- accepts digits, decimals and '?', surrounded by ()
-
-//what to do when DOM loads
-$(function(){
-        //watch filtering
-        $('.js-toggle-label-filter, .js-select-member, .js-due-filter, .js-clear-all').live('mouseup', calcListPoints);
-        $('.js-input').live('keyup', calcListPoints);
-
-        calcListPoints();
-
+$(function() {
+    $('.js-toggle-label-filter, .js-select-member, .js-due-filter, .js-clear-all').live('mouseup', showLabels);
+    $('.js-input').live('keyup', showLabels);
+    showLabels();
 });
 
-document.body.addEventListener('DOMNodeInserted',function(e){
-        if(e.target.id=='board') setTimeout(calcListPoints);
-        else if($(e.target).hasClass('list')) calcListPoints();
+document.body.addEventListener('DOMNodeInserted', function(e) {
+    if (e.target.id == 'board') 
+        setTimeout(showLabels);
+    else if ($(e.target).hasClass('list')) 
+        showLabels();
 });
 
-//calculate list totals
-var ltooo;
-function calcListPoints(){
-        clearTimeout(ltooo);
-        ltooo = setTimeout(function(){
-                $('.list').each(function(){
-                        if(!this.list) new List(this);
-                        else if(this.list.calc) this.list.calc();
-                });
+var labelTimeout;
+function showLabels() {
+    clearTimeout(labelTimeout);
+    labelTimeout = setTimeout(function() {
+        $('.list').each(function() {
+            if (!this.list) 
+                new List(this);
         });
+    });
 };
 
-//.list pseudo
-function List(el){
-        if(el.list)return;
-        el.list=this;
-
-        var $list=$(el),
-                busy = false,
-                to,
-                to2;
-
-        function readCard($c){
-                if($c.target) {
-                        if(!/list-card/.test($c.target.className)) return;
-                        $c = $($c.target).filter('.list-card:not(.placeholder)');
-                }
-                $c.each(function(){
-                        if(!this.listCard)
-                            new ListCard(this,'points');
-                        else
-                            setTimeout(this.listCard['points'].refresh);
-                });
-        };
-
-        this.calc = function(e){
-                if(e&&e.target&&!$(e.target).hasClass('list-card')) return;
-                clearTimeout(to);
-                to = setTimeout(function(){
-                        var score=0,
-                                        attr = 'points';
-                                $list.find('.list-card:not(.placeholder):visible').each(function(){
-                                        if(!this.listCard) return;
-                                        if(!isNaN(Number(this.listCard[attr].project)))score+=Number(this.listCard[attr].project)
-                                });
-                });
-        };
-
-        $list.on('DOMNodeRemoved',this.calc).on('DOMNodeInserted',readCard);
-
-        setTimeout(function(){
-                readCard($list.find('.list-card'));
-                setTimeout(el.list.calc);
-        });
-};
-
-//.list-card pseudo
-function ListCard(el, identifier){
-        if(el.listCard && el.listCard[identifier]) return;
-
-        //lazily create object
-        if (!el.listCard){
-                el.listCard={};
+function List(el) {
+    if (el.list)
+        return;
+    el.list = this;
+    
+    var $list = $(el);
+    var busy = false;
+    var to;
+    var to2;
+    
+    function readCard($c) {
+        if ($c.target) {
+            if (!/list-card/.test($c.target.className)) 
+                return;
+            $c = $($c.target).filter('.list-card:not(.placeholder)');
         }
-        el.listCard[identifier]=this;
-
-        var points=-1,
-                consumed=false,
-                regexp=regp,
-                parsed,
-                that=this,
-                busy=false,
-                ptitle='',
-                $card=$(el),
-                $badge=$('<div class="badge project" />'),
-                to,
-                to2;
-
-        this.refresh=function(){
-                if(busy) return;
-                busy = true;
-                clearTimeout(to);
-                to = setTimeout(function(){
-                        var $title=$card.find('a.list-card-title');
-                        if(!$title[0])return;
-                        var title=$title[0].childNodes[1].textContent;
-                        if(title) el._title = $title;
-                        if(title!=ptitle) {
-                                ptitle = title;
-                                parsed=title.match(regexp);
-                                points=parsed?parsed[2]:-1;
-                        }
-                        clearTimeout(to2);
-                        to2 = setTimeout(function(){
-                                $badge
-                                        .text(that.points)
-                                        .prependTo($card.find('.badges'));
-
-                                $title[0].childNodes[1].textContent = el._title = $.trim(el._title[0].text.replace(regp,'$1'));
-                                var list = $card.closest('.list');
-                                if(list[0]) list[0].list.calc();
-                                busy = false;
-                        })
-                });
-        };
-
-        this.__defineGetter__('points',function(){
-                return parsed?points:''
+        $c.each(function() {
+            if (!this.listCard)
+                new ListCard(this);
+            else
+                setTimeout(this.listCard.refresh);
         });
+    };
+    
+    $list.on('DOMNodeInserted', readCard);
+    
+    setTimeout(function() {
+        readCard($list.find('.list-card'));
+    });
+};
 
-        if(!consumed) el.addEventListener('DOMNodeInserted',function(e){
-                if(/card-short-id/.test(e.target.className) && !busy)
-                        that.refresh();
+function ListCard(el) {
+    if (el.listCard) 
+        return;
+    el.listCard = this;
+    
+    var regexp = /((?:^|\s))\{([a-zA-Z0-9\s]+)(\})\s?/m;
+    var label = -1;
+    var parsed;
+    var that = this;
+    var busy = false;
+    var ptitle = '';
+    var $card = $(el);
+    var $badge = $('<div class="badge project" />');
+    var to;
+    var to2;
+
+    this.refresh = function() {
+        if (busy) 
+            return;
+        busy = true;
+        
+        clearTimeout(to);
+        to = setTimeout(function() {
+            var $title = $card.find('a.list-card-title');
+            if(!$title[0])
+                return;
+            
+            var title = $title[0].childNodes[1].textContent;
+            if (title) 
+                el._title = $title;
+            
+            if (title != ptitle) {
+                ptitle = title;
+                parsed = title.match(regexp);
+                label = parsed ? parsed[2] : -1;
+            }
+            
+            clearTimeout(to2);
+            
+            to2 = setTimeout(function() {
+                $badge.text(that.label).prependTo($card.find('.badges'));
+                $title[0].childNodes[1].textContent = el._title = $.trim(el._title[0].text.replace(regexp,'$1'));
+                var list = $card.closest('.list');
+                busy = false;
+            })
         });
+    };
 
-        setTimeout(that.refresh);
+    this.__defineGetter__('label', function() {
+        return parsed ? label : '';
+    });
+    
+    el.addEventListener('DOMNodeInserted', function(e) {
+        if (/card-short-id/.test(e.target.className) && !busy)
+            that.refresh();
+    });
+    
+    setTimeout(that.refresh);
 };
