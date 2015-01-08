3a86e876982ec138b4cd3a45d13a1a9fd236fab7
Luís A. Bastião Silva <luis.kop@gmail.com>
2015-01-08 22:50:03 +0000

    Fixed options requested in #11

--- options.js
+++ options.js
@@ -4,6 +4,10 @@ function save_options() {
   var colorHex = document.getElementById('colorHex').value;
   var _tmp = {}
   _tmp[projectName] = colorHex
+  var node = document.createElement("LI");
+  node.innerHTML = "<li>" + projectName + ": " + colorHex + "</li>";
+  document.getElementById("list").appendChild(node);
+
   chrome.storage.sync.set(_tmp, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -14,15 +18,32 @@ function save_options() {
   });
 }
 
+function reset_options () {
+	chrome.storage.sync.clear(function () {
+		document.getElementById("list").innerHTML = ""
+	});
+}
+
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get(null, function(items) {
     // TODO: work on
-    console.log(items);
+    for (item in items)
+    {
+	var node = document.createElement("LI");  
+	node.innerHTML = "<li>" + item + ": " + items[item] + "</li>";
+        document.getElementById("list").appendChild(node);
+
+
+    }
+
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
     save_options);
+
+document.getElementById('reset').addEventListener('click',
+    reset_options);
