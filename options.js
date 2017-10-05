a52c5b2d634f0d15f77ef9aabadeab3041445c82
Skylar Bolton <sbolton@bcimedia.com>
2017-10-05 11:09:18 -0600

    Into Popup for Lable changes
    Added Jquery and minor styling

--- options.js
+++ options.js
@@ -6,35 +6,37 @@ function save_options() {
     var _tmp = {}
     _tmp[projectName] = colorHex
     chrome.storage.sync.set(_tmp, function(){ });
-    document.getElementById("list").innerHTML = '';
     restore_options()
   }else{
     alert("Must specify a Project name and Color");
   }
 }
 
-function reset_options () {
-  chrome.storage.sync.clear(function () {
-    document.getElementById("list").innerHTML = ""
+function delete_option(option) {
+  chrome.storage.sync.remove(option, function () {
+    restore_options();
   });
-}
+};
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
+    document.getElementById("list").innerHTML = '';
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get(null, function(items) {
     // TODO: work on
     for (item in items) {
-      var node = document.createElement("LI");
-      node.innerHTML = "<li>" + item + ": <span style='color:"+items[item]+";'>" + items[item] + "</span></li>";
+      var node = document.createElement("div");
+      node.innerHTML = "<span class='color-swath' style='background:"+items[item]+";'></span>"+
+        "<label class='project-name'>" + item + "</label>" +
+        "<button class='delete-button' data-name='"+item+"'>X</button>";
       document.getElementById("list").appendChild(node);
     }
+    $(".delete-button").click(function(){
+      console.log($(this).data("name"));
+      delete_option($(this).data("name"));
+    });
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-                                                 save_options);
-
-document.getElementById('reset').addEventListener('click',
-                                                  reset_options);
+document.getElementById('save').addEventListener('click', save_options);
