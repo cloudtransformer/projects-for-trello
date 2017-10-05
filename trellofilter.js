e6f35cf2e4335940e79f167f23f640c61781ec27
Skylar Bolton <sbolton@bcimedia.com>
2017-10-05 11:22:55 -0600

    Clean up and formatting

--- trellofilter.js
+++ trellofilter.js
@@ -1,21 +1,21 @@
 $(function() {
-$(".board-header").append('<a id="filterTrello" class="s4tLink quiet ed board-header-btn dark-hover" href="#"><span class="icon-sm board-header-btn-icon"></span><span class="text board-header-btn-text">Filter</span></a>');
+  $(".board-header").append('<a id="filterTrello" class="s4tLink quiet ed board-header-btn dark-hover" href="#"><span class="icon-sm board-header-btn-icon"></span><span class="text board-header-btn-text">Filter</span></a>');
 
-$( "#filterTrello" ).click(function() {
-  var _tTmp = window.prompt("Insert the terms to filter","ALL");
-  console.log(_tTmp);
-  if (_tTmp=="ALL")
-  {
-    var divs = $(".list-card").show();
-  }
-  else
-  {
-    var divs = $(".list-card").hide();
-    var divs = $(".list-card:has(."+_tTmp+")").show();
+  $( "#filterTrello" ).click(function() {
+    var _tTmp = window.prompt("Insert the terms to filter","ALL");
+    console.log(_tTmp);
+    if (_tTmp=="ALL")
+      {
+        var divs = $(".list-card").show();
+      }
+      else
+        {
+          var divs = $(".list-card").hide();
+          var divs = $(".list-card:has(."+_tTmp+")").show();
 
-  }
+        }
 
-});
+  });
 
 });
 
--- trelloprojects.js
+++ trelloprojects.js
@@ -1,7 +1,7 @@
 $(function() {
-    $('.js-toggle-label-filter, .js-select-member, .js-due-filter, .js-clear-all').live('mouseup', showLabels);
-    $('.js-input').live('keyup', showLabels);
-    showLabels();
+  $('.js-toggle-label-filter, .js-select-member, .js-due-filter, .js-clear-all').live('mouseup', showLabels);
+  $('.js-input').live('keyup', showLabels);
+  showLabels();
 });
 
 
@@ -11,187 +11,187 @@ var usedColors = colorList.slice(0);
 
 
 document.body.addEventListener('DOMNodeInserted', function(e) {
-    if (e.target.id == 'board')
-        setTimeout(showLabels);
-    else if ($(e.target).hasClass('list'))
-        showLabels();
+  if (e.target.id == 'board')
+    setTimeout(showLabels);
+  else if ($(e.target).hasClass('list'))
+  showLabels();
 });
 
 var labelTimeout;
 function showLabels() {
-    clearTimeout(labelTimeout);
-    labelTimeout = setTimeout(function() {
-        $('.list').each(function() {
-            if (!this.list)
-                new List(this);
-        });
+  clearTimeout(labelTimeout);
+  labelTimeout = setTimeout(function() {
+    $('.list').each(function() {
+      if (!this.list)
+        new List(this);
     });
+  });
 };
 
 //+ Jonas Raoni Soares Silva
 //@ http://jsfromhell.com/array/shuffle [v1.0]
 function shuffle(o){ //v1.0
-    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
+  for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+  return o;
 };
 
 
 function List(el) {
-    if (el.list)
+  if (el.list)
+    return;
+  el.list = this;
+
+  var $list = $(el);
+  var busy = false;
+
+  function readCard($c) {
+    if ($c.target) {
+      if (!/list-card/.test($c.target.className))
         return;
-    el.list = this;
-
-    var $list = $(el);
-    var busy = false;
-
-    function readCard($c) {
-        if ($c.target) {
-            if (!/list-card/.test($c.target.className))
-                return;
-            $c = $($c.target).filter('.list-card:not(.placeholder)');
-        }
-        $c.each(function() {
-            if (!this.listCard)
-                new ListCard(this);
-            else
-                setTimeout(this.listCard.refresh);
-        });
-    };
-
-    $list.on('DOMNodeInserted', readCard);
-
-    setTimeout(function() {
-        readCard($list.find('.list-card'));
+      $c = $($c.target).filter('.list-card:not(.placeholder)');
+    }
+    $c.each(function() {
+      if (!this.listCard)
+        new ListCard(this);
+      else
+        setTimeout(this.listCard.refresh);
     });
+  };
+
+  $list.on('DOMNodeInserted', readCard);
+
+  setTimeout(function() {
+    readCard($list.find('.list-card'));
+  });
 };
 
 function ListCard(el) {
-    if (el.listCard)
+  if (el.listCard)
+    return;
+  el.listCard = this;
+
+  var regexp = /\{([^{}]+)\}/;
+  var label = -1;
+  var parsed;
+  var that = this;
+  var busy = false;
+  var ptitle = '';
+  var $card = $(el);
+  var to;
+  var to2;
+
+  this.refresh = function() {
+    if (busy)
+      return;
+    busy = true;
+
+    $card.find(".project").remove();
+
+    clearTimeout(to);
+    to = setTimeout(function() {
+      var $title=$card.find('span.list-card-title');
+      if(!$title[0])
         return;
-    el.listCard = this;
 
-    var regexp = /\{([^{}]+)\}/;
-    var label = -1;
-    var parsed;
-    var that = this;
-    var busy = false;
-    var ptitle = '';
-    var $card = $(el);
-    var to;
-    var to2;
-
-    this.refresh = function() {
-        if (busy)
-            return;
-        busy = true;
-
-        $card.find(".project").remove();
-
-        clearTimeout(to);
-        to = setTimeout(function() {
-            var $title=$card.find('span.list-card-title');
-            if(!$title[0])
-                return;
-
-            // var title = $title[0].innerText;
+      // var title = $title[0].innerText;
       var title = $title[0].childNodes[1].textContent;
       if (title)
-                el._title = title;
+        el._title = title;
 
       var ptitle = $title.data('parsed-title');
       if (title != ptitle){
-                if(title != ptitle){
-                           $title.data('orig-title', title); // store the non-mutilated title (with all of the estimates/time-spent in it).
-                        }
-                parsed=title.match(regexp);
-                label=parsed ? parsed : -1;
-            } else {
-                var origTitle = $title.data('orig-title');
-                el._title = origTitle;
-                parsed=origTitle.match(regexp);
-                label=parsed ? parsed : -1;
-            }
+        if(title != ptitle){
+          $title.data('orig-title', title); // store the non-mutilated title (with all of the estimates/time-spent in it).
+        }
+        parsed=title.match(regexp);
+        label=parsed ? parsed : -1;
+      } else {
+        var origTitle = $title.data('orig-title');
+        el._title = origTitle;
+        parsed=origTitle.match(regexp);
+        label=parsed ? parsed : -1;
+      }
 
-            clearTimeout(to2);
+      clearTimeout(to2);
 
-            to2 = setTimeout(function() {
-                function recursiveReplace() {
-                    if (label != -1) {
+      to2 = setTimeout(function() {
+        function recursiveReplace() {
+          if (label != -1) {
 
-                        var tmp = $('<div class="badge project""  />');
+            var tmp = $('<div class="badge project""  />');
 
-                        tmp.text(that.label[1]).appendTo($card.find('.badges'));
-                        ptitle = $.trim(el._title.replace(label[0],''));
-                        el._title = ptitle;
-                        $title.data('parsed-title', ptitle);
-                        // $title[0].innerText = ptitle;
-                        $title[0].childNodes[1].textContent = ptitle;
-                        parsed = ptitle.match(regexp);
-                        label = parsed ? parsed : -1;
-
-
-
-                        if (tmp!=undefined && tmp.text()!="")
-                        {
-                            tmp.addClass(tmp.text());
-                            var _tmp = {};
-                            _tmp[tmp.text()] = defaultColor;
-
-                            chrome.storage.sync.get(_tmp,
-                                function(items) {
-                                // default color #801b00
-
-                                var colorSet = items[tmp.text()];
-                                if (items[tmp.text()]==defaultColor)
-                                {
-
-                                    var colorSet=usedColors.pop();
-
-                                    if (colorSet==undefined){
-                                        usedColors = shuffle(colorList.slice(0));
-                                        colorSet = usedColors.pop();
-
-                                    }
-                                    var _tmp = {}
-                                    _tmp[tmp.text()] = colorSet;
-                                    chrome.storage.sync.set(_tmp, function() {
-
-                                      });
-
-                                }
- if (tmp!=undefined)
-                                    tmp.attr("style", "background-color: " + colorSet+ " !important");
-                              });
-                        }
-
-
-                        //tmp.css("background-color", colorArray[tmp.text()] );
-                        if (label != -1) {
-                            el._title = ptitle;
-                            recursiveReplace();
-                        }
+            tmp.text(that.label[1]).appendTo($card.find('.badges'));
+            ptitle = $.trim(el._title.replace(label[0],''));
+            el._title = ptitle;
+            $title.data('parsed-title', ptitle);
+            // $title[0].innerText = ptitle;
+            $title[0].childNodes[1].textContent = ptitle;
+            parsed = ptitle.match(regexp);
+            label = parsed ? parsed : -1;
 
 
 
-                    }
-                };
+            if (tmp!=undefined && tmp.text()!="")
+              {
+                tmp.addClass(tmp.text());
+                var _tmp = {};
+                _tmp[tmp.text()] = defaultColor;
+
+                chrome.storage.sync.get(_tmp,
+                                        function(items) {
+                                          // default color #801b00
+
+                                          var colorSet = items[tmp.text()];
+                                          if (items[tmp.text()]==defaultColor)
+                                            {
+
+                                              var colorSet=usedColors.pop();
+
+                                              if (colorSet==undefined){
+                                                usedColors = shuffle(colorList.slice(0));
+                                                colorSet = usedColors.pop();
+
+                                              }
+                                              var _tmp = {}
+                                              _tmp[tmp.text()] = colorSet;
+                                              chrome.storage.sync.set(_tmp, function() {
+
+                                              });
+
+                                            }
+                                            if (tmp!=undefined)
+                                              tmp.attr("style", "background-color: " + colorSet+ " !important");
+                                        });
+              }
+
+
+              //tmp.css("background-color", colorArray[tmp.text()] );
+              if (label != -1) {
+                el._title = ptitle;
                 recursiveReplace();
-
-                var list = $card.closest('.list');
-                busy = false;
-            })
-        });
-    };
+              }
 
 
-    this.__defineGetter__('label', function() {
-        return parsed ? label : '';
+
+          }
+        };
+        recursiveReplace();
+
+        var list = $card.closest('.list');
+        busy = false;
+      })
     });
+  };
 
-    el.addEventListener('DOMNodeInserted', function(e) {
-        if (/card-short-id/.test(e.target.className) && !busy)
-            that.refresh();
-    });
 
-    setTimeout(that.refresh);
+  this.__defineGetter__('label', function() {
+    return parsed ? label : '';
+  });
+
+  el.addEventListener('DOMNodeInserted', function(e) {
+    if (/card-short-id/.test(e.target.className) && !busy)
+      that.refresh();
+  });
+
+  setTimeout(that.refresh);
 };
